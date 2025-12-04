6476d48bf3604179db406be770e9847cf8f9004c
kazmi <smhk760@gmail.com>
2025-12-04 17:41:24 +0500

    fix-text

--- lib/commands/greetings.tsx
+++ lib/commands/greetings.tsx
@@ -83,7 +83,7 @@ export class MakiZeninCommand implements Command {
 
          ⚔️  ✨  ⚔️
         ╔═══════════╗
-        ║ MAKI-CHAN ║
+        ║ MAKI-Zenin║
         ╚═══════════╝
          ⚔️  ✨  ⚔️
 
